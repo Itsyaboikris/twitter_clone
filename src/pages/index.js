ce78e61619189f628e9c37da4c40be7e3d6f24a8
itsyaboikris <kristoff_riley@outlook.com>
2023-04-29 16:12:34 -0400

    created widget component and search bar

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Sidebar from '@/components/Sidebar'
 import Feed from '@/components/Feed'
+import Widgets from '@/components/Widgets'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,6 +11,7 @@ export default function Home() {
 		<main className='flex min-h-screen max-w-7xl mx-auto'>
 			<Sidebar/>
 			<Feed/>
+			<Widgets/>
 		</main>
 	)
 }
