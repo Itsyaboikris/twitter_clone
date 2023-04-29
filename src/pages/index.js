b6a9c70a7892a94a7d403d0a98be532bd6188c2b
itsyaboikris <kristoff_riley@outlook.com>
2023-04-29 04:45:40 -0400

    Feed component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Sidebar from '@/components/Sidebar'
+import Feed from '@/components/Feed'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,6 +9,7 @@ export default function Home() {
 	return (
 		<main className='flex min-h-screen max-w-7xl mx-auto'>
 			<Sidebar/>
+			<Feed/>
 		</main>
 	)
 }
