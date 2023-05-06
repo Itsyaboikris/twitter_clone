3e3e62bff5d4f21d7ed9159db7709924b13b08b2
itsyaboikris <kristoff_riley@outlook.com>
2023-05-05 23:10:12 -0400

    who to follow + animations with framer motion

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,12 +6,12 @@ import Widgets from '@/components/Widgets'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home({newsResults}) {
+export default function Home({newsResults, randomUsersResults}) {
 	return (
 		<main className='flex min-h-screen mx-auto'>
 			<Sidebar/>
 			<Feed/>
-			<Widgets newsResults={newsResults.articles} />
+			<Widgets newsResults={newsResults.articles} randomUsersResults={randomUsersResults?.results || null} />
 		</main>
 	)
 }
@@ -21,9 +21,22 @@ export default function Home({newsResults}) {
 export async function getServerSideProps() {
 	const newsResults = await fetch ("https://saurav.tech/NewsAPI/top-headlines/category/business/us.json").then ((res) => res.json())
 
+	let randomUsersResults = [];
+
+	try {
+		const res = await fetch(
+		  "https://randomuser.me/api/?results=30&inc=name,login,picture"
+		);
+	
+		randomUsersResults = await res.json();
+	  } catch (e) {
+		randomUsersResults = [];
+	}
+
 	return{
 		props: {
-			newsResults
+			newsResults,
+			randomUsersResults
 		}
 	}
 }
